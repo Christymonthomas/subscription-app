d77261958b8a71bd5b56aafa6786453eea458291
Christy <christymonthomas1999@gmail.com>
2024-06-22 11:43:00 +0530

    Added validations

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,18 @@
-export const register = (req, res) => {
-  res.json({
-    data: "Hey you reached node server - this is from controllers",
-  });
+export const register = async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    if (!name) {
+      return res.status(400).json({ error: "Name is required" });
+    }
+    if (!password || password.length < 6) {
+      return res.status(400).json({
+        error: "Password is required and shpuld be 6 characters long",
+      });
+    }
+    res.json({
+      data: "This is /register endpoint",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
